c21058699549582172a90df4aad7b71ffed07846
LizaAvramenko <avramenkoyelyzaveta09@gmail.com>
2022-08-11 22:16:07 +0300

    added second homework

---
+++ Homeworks/ДЗ 2/index.js
@@ -0,0 +1,78 @@
+
+
+
+//4
+ 
+// let array1 = [1, 2, 3, 4, 5, 6];
+// let array2 = [25, 7, 9, 85, 77, 4]
+
+
+// function sum(arr) {
+//     let sum = 0
+//     for (let i=0; i < arr.length; i++) {
+//     sum += arr[i] 
+//     }
+//    return sum
+// }
+
+// function comparison (arr1, arr2) {
+//     if ( sum(array1) > sum(array2)) {
+//         console.log ('Сумма чисел первого массива больше')
+//     }
+//     else {
+//         console.log ('Сумма чисел второго массива больше')
+//     }
+// }
+// console.log (comparison ( array1, array2))
+
+
+//5. Написать функцию, которая принимает массив строк. И возвращает саммую длинную строку в этом массиве.
+
+
+
+// let string = [ 'fghfhf', 'ttd', 'fdfdfduioop', 'op', 'p' ];
+
+// function longestStr (str) {
+//     maxString = [];
+//     for (let i=0; i<str.length; i++) {
+//         if (maxString.length < str[i].length) {
+//             maxString = str [i];
+//         }
+//     }
+//     return maxString;
+// }
+// console.log (longestStr(string))
+
+//6
+
+// const num1 = +prompt('Введите первое число');
+// const num2 = +prompt('Введите второе число');
+
+// if ( isNaN(num1) || isNaN(num2) ) {
+//     alert ('Not correct')
+//    }
+//    else {
+//       getNumberSum(num1, num2)
+//    }
+//    console.log 
+   
+// function getNumberSum (number1, number2) {
+// let result = number1 + number2
+// alert (result)
+// }
+     
+//7
+
+// function checkType (tpof) {
+//     return (typeof(tpof))
+// }
+
+// console.log (checkType(true))
+
+//8
+
+// function factorial(n) {
+//     return (n != 1) ? n * factorial(n - 1) : 1;
+//   }
+      
+//   console.log( factorial(3) );
